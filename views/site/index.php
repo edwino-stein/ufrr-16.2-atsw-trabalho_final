301bb8448f8c31ee6257d136ce40cbed769fac16
Edwino Stein <edwino.stein@gmail.com>
2017-01-28 18:29:51 -0400

    Adicionando ideia inicial para tela de Busca

--- views/site/index.php
+++ views/site/index.php
@@ -13,9 +13,47 @@ $this->registerJsFile($baseUrl.'/js/app/app.js');
         Home
     </div>
     <div role="tabpanel" class="tab-pane" id="query">
-        query
+        <div class="page-header">
+          <h2>Buscar por algum tema</h2>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="query"></div>
+            </div>
+            <div class="panel-heading report">
+                Total de <strong class="counter">0</strong> tweet(s)
+            </div>
+            <div class="panel-body message">
+                <h1>Nenhum resultado</h1>
+            </div>
+            <ul class="list-group results">
+                <li class="list-group-item">
+                    <h4>Tweet text 1</h4>
+                </li>
+                <li class="list-group-item">
+                    <h4>Tweet text 2</h4>
+                </li>
+                <li class="list-group-item">
+                    <h4>Tweet text 3</h4>
+                </li>
+            </ul>
+        </div>
     </div>
     <div role="tabpanel" class="tab-pane" id="wordcounter">
         wordcounter
     </div>
+</div>
+
+<div id="query-input-abstract">
+    <form>
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Buscar...">
+            <span class="input-group-btn">
+                <button class="btn btn-primary" type="submit">
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </button>
+            </span>
+        </div>
+    </form>
 </div>
--- web/js/app/View/Query.js
+++ web/js/app/View/Query.js
@@ -1,6 +1,8 @@
 App.define('View.Query',{
 
     $domObj: '#query',
+    queryInput: 'query-main',
+    queryUrl: 'twitter/query',
 
     onSelected: function(id, obj, e){
         console.log('selected Query');
@@ -9,6 +11,23 @@ App.define('View.Query',{
     onDeselected: function(id, obj, e){
         console.log('deselected Query');
     },
+
+    render: function(){
+        var me = this;
+        me.queryInput = this._appRoot_.get('Util.Classes').getInstance('QueryInput', this.$domObj.find('.query'), {
+            id: me.queryInput,
+            href: me.queryUrl,
+            placeholder: 'Buscar tema...',
+            onQuery: function(){
+                console.log(arguments);
+            },
+            onQueryError: function(){
+                console.log(arguments);
+            }
+        });
+    },
+
+
 
     ready: function(){ this.callSuper(); },
     init: function(){ this.callSuper(); }
