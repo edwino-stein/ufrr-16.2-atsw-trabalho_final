93a11972da4ef20e070e741c36730b82acace696
Edwino Stein <edwino.stein@gmail.com>
2017-01-28 18:27:35 -0400

    Adicionando widget para pesquisa via ajax

--- web/js/app/Util/Classes.js
+++ web/js/app/Util/Classes.js
@@ -73,6 +73,86 @@ App.define('Util.Classes',{
         };
     },
 
+    QueryInput: function QueryInput (renderTo, config){
+
+        if(typeof(QueryInput.initted) !== 'bool' || !QueryInput.initted){
+            QueryInput.domObj = $('#query-input-abstract').removeAttr('id').remove();
+            QueryInput.instanceCounter = 0;
+            QueryInput.initted = true;
+        }
+
+        var me = this;
+        me.config = typeof(config) !== 'object' ? {} : config;
+        me.config.id = typeof(config.id) !== 'string' ? 'query-input-' + QueryInput.instanceCounter : config.id;
+        me.config.href = typeof(config.href) !== 'string' ? '' : config.href;
+        me.config.method = typeof(config.method) !== 'string' ? 'GET' : config.method;
+        me.config.dataType = typeof(config.dataType) !== 'string' ? 'json' : config.dataType;
+        me.config.placeholder = !config.placeholder ? "Buscar..." : config.placeholder;
+        me.config.maxCount = !config.maxCount ? 15 : config.maxCount;
+        me.config.onBeforeQuery = typeof(config.onBeforeQuery) !== 'function' ? function(){ me.disableQuery(true); } : config.onBeforeQuery;
+        me.config.onQuery = typeof(config.onQuery) !== 'function' ? function(){} : config.onQuery;
+        me.config.onAfterQuery = typeof(config.onAfterQuery) !== 'function' ? function(){  me.disableQuery(false); } : config.onAfterQuery;
+        me.config.onQueryError = typeof(config.onQueryError) !== 'function' ? function(){ console.log(arguments); } : config.onQueryError;
+
+        me.domObj = QueryInput.domObj.clone();
+        me.disabled = false;
+
+        me.disableQuery = function(disabled){
+            me.domObj.find('input[type=text]').prop('disabled', disabled);
+            me.domObj.find('button').prop('disabled', disabled);
+            me.disabled = disabled;
+        };
+
+        me.hasDisabled = function(){
+            return me.disabled;
+        };
+
+        me.getValue = function(){
+            return me.domObj.find('input[type=text]').val();
+        };
+
+        me.setValue = function (value){
+            me.domObj.find('input[type=text]').val(value);
+        };
+
+        me.setPlaceholder = function(placeholder){
+            me.domObj.find('input[type=text]').attr('placeholder', placeholder);
+        };
+
+        me.doQuery = function(value){
+            me.setValue(value);
+            me.domObj.submit();
+        };
+
+        me.domObj.submit(function(e){
+            e.preventDefault();
+
+            var data = me.getValue();
+            if(data === '') return;
+
+            me.config.onBeforeQuery(e);
+            var ajax = QueryInput.classScope._appRoot_.get('Util.Ajax');
+            ajax.request({
+                url: me.config.href,
+                data: {
+                    q: data,
+                    count: me.config.maxCount
+                },
+                method: me.config.method,
+                dataType: me.config.dataType,
+                success: me.config.onQuery,
+                fail: me.config.onQueryError,
+                complete: me.config.onAfterQuery
+            });
+        });
+
+        me.domObj.attr('id', me.config.id);
+        me.setPlaceholder(me.config.placeholder);
+        $(renderTo).append(me.domObj);
+        me.disableQuery(false);
+        QueryInput.instanceCounter++;
+    },
+
     getInstance: function(className){
 
         if(this.instantiable.indexOf(className) < 0)
@@ -89,8 +169,11 @@ App.define('Util.Classes',{
         var unInstantiable = ['_appRoot_', '_initted_', '_namespace_', '_parent_', '_super_', 'apply', 'hasProperty', 'init', 'ready', 'getInstance', 'instantiable'];
 
         for(var i in this){
-            if(unInstantiable.indexOf(i) < 0)
+            if(unInstantiable.indexOf(i) < 0){
                 this.instantiable.push(i);
+                this[i].classScope = this;
+            }
+
         }
     }
 });
